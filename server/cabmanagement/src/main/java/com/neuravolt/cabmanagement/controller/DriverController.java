d9bf2f4686ba27a5883a9748e276630eaff663d6
Abhinand Manwate <abhinandmanwate123@gmail.com>
2023-07-09 20:32:19 +0530

    DriverController updated

--- server/cabmanagement/src/main/java/com/neuravolt/cabmanagement/controller/DriverController.java
+++ server/cabmanagement/src/main/java/com/neuravolt/cabmanagement/controller/DriverController.java
@@ -1,6 +1,8 @@
 package com.neuravolt.cabmanagement.controller;
+import com.neuravolt.cabmanagement.model.Cab;
 import com.neuravolt.cabmanagement.model.Driver;
 import com.neuravolt.cabmanagement.service.DriverService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -23,7 +25,7 @@ public class DriverController {
     }
 
     //Give All driver details no need to pass Driver Id
-    @GetMapping()
+    @GetMapping
     public List<Driver> getDriverDetails(){
         return  driverService.getAllDriver();
     }
@@ -35,11 +37,59 @@ public class DriverController {
         driverService.updateDriver(driver);
         return "Driver Details updated successfully!";
     }
-    @DeleteMapping("{DriverIdNumber}")
-    public String DeleteDriverDetails(@PathVariable("DriverIdNumber") String DriverIdNumber){
+    @DeleteMapping("{driverIdNumber}")
+    public String DeleteDriverDetails(@PathVariable("driverIdNumber") String driverIdNumber){
 
-        driverService.deleteDriver(DriverIdNumber);
+        driverService.deleteDriver(driverIdNumber);
         return " Driver Details deleted successfully!";
     }
+
+    //Statement 3 code
+    @PostMapping("/{driverIdNumber}/cab/{cabRegistrationNumber}")
+    public ResponseEntity<String> assignCabToDriver(
+            @PathVariable("driverIdNumber") String driverIdNumber,
+            @PathVariable("cabRegistrationNumber") String cabRegistrationNumber) {
+        Driver driver = driverService.assignCabToDriver(driverIdNumber, cabRegistrationNumber);
+        if (driver != null) {
+            return ResponseEntity.ok("Cab assigned to driver successfully");
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/{driverIdNumber}/cab/{cabRegistrationNumber}")
+    public ResponseEntity<String> updateAssignedCab(
+            @PathVariable("driverIdNumber") String driverIdNumber,
+            @PathVariable("cabRegistrationNumber") String cabRegistrationNumber) {
+        Driver driver = driverService.updateAssignedCab(driverIdNumber, cabRegistrationNumber);
+        if (driver != null) {
+            return ResponseEntity.ok("Assigned cab updated successfully");
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/{driverIdNumber}/cab")
+    public ResponseEntity<Cab> getAssignedCab(
+            @PathVariable("driverIdNumber") String driverIdNumber) {
+        Cab cab = driverService.getAssignedCab(driverIdNumber);
+        if (cab != null) {
+            return ResponseEntity.ok(cab);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{driverIdNumber}/cab")
+    public ResponseEntity<String> removeAssignedCab(
+            @PathVariable("driverIdNumber") String driverIdNumber) {
+        boolean removed = driverService.removeAssignedCab(driverIdNumber);
+        if (removed) {
+            return ResponseEntity.ok("Assigned cab removed successfully");
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 
 }
