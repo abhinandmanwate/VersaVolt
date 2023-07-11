60f40193dc58bbf1d7fd43e5e9b11c6c255a9c98
Aditya Bile <adityabile30602@gmail.com>
2023-07-11 13:39:14 +0530

    Add frontend folders

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import DTable from "./components/driverInfo/Dtable"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi</h1>
+      <DTable/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/driverInfo/Dtable.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Dtable() {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  )
+}
+
+export default Dtable
