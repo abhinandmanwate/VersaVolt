aeeaba09212ec61c135888992c6ebe141aa5150b
PrasannaBH <bhilegaonkarprasanna12@gmail.com>
2023-07-13 12:50:40 +0530

    Added UPDATE Driver

--- client/src/components/driverInfo/Dtable.js
+++ client/src/components/driverInfo/Dtable.js
@@ -9,10 +9,11 @@ const Dtable = ({ rows, deleteRow, editRow }) => {
       <table className="table">
         <thead>
           <tr>
-            <th>Id</th>
-            <th>Name</th>
-            <th className="expand">Email</th>
-            <th>Mobile</th>
+            {/* <th>Sr No.</th> */}
+            <th>Driver Id Number</th>
+            <th>Driver Name</th>
+            <th className="expand">Driver Email</th>
+            <th>Driver Phone Number</th>
             <th>Action</th>
           </tr>
         </thead>
@@ -25,6 +26,7 @@ const Dtable = ({ rows, deleteRow, editRow }) => {
                 <td data-title="Driver Email" className="expand">{row.email}</td>
                 <td data-title="Driver Mobile">{row.mobile}</td>
                 <td data-title="Action"> */}
+                {/* <td>{idx + 1}</td> */}
                 <td>{row.driverIdNumber}</td>
                 <td>{row.driverName}</td>
                 <td className="expand">{row.driverEmail}</td>
